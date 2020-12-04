9c62ce42b19bc109dcf9dd2a14c9abcb13361426
cuixiaoyan <56696508@qq.com>
2020-12-04 22:10:30 +0800

    稀疏数组解析完成

--- src/main/java/com/cxy/algorithm/sparseAlgorithm.java
+++ src/main/java/com/cxy/algorithm/sparseAlgorithm.java
@@ -29,12 +29,12 @@ public class sparseAlgorithm {
         int sum = 0;
         for (int i = 0; i < 11; i++) {
             for (int j = 0; j < 11; j++) {
-                if(chessArr[i][j] != 0)
+                if (chessArr[i][j] != 0)
                     sum++;
             }
         }
         //2、创建对应的稀疏数组
-        int sparseArr[][] = new int [sum + 1][3];
+        int sparseArr[][] = new int[sum + 1][3];
         //给稀疏数组赋值。
         sparseArr[0][0] = 11;
         sparseArr[0][1] = 11;
@@ -44,11 +44,11 @@ public class sparseAlgorithm {
         int count = 0;
         for (int i = 0; i < 11; i++) {
             for (int j = 0; j < 11; j++) {
-                if (chessArr[i][j] != 0){
+                if (chessArr[i][j] != 0) {
                     count++;
                     sparseArr[count][0] = i;
                     sparseArr[count][1] = j;
-                    sparseArr[count][2] = chessArr [i][j];
+                    sparseArr[count][2] = chessArr[i][j];
                 }
             }
         }
@@ -59,9 +59,24 @@ public class sparseAlgorithm {
             System.out.printf("%d\t%d\t%d\t\n", sparseArr[i][0], sparseArr[i][1], sparseArr[i][2]);
         }
         System.out.println();
-
-
-
+        //将稀疏数组，恢复为原始的二维数组。
+        /**
+         * 1、先读取稀疏数组的第一行，根据第一行的数据，创建原始的二维数组。
+         * 2、在读取稀疏数组后几行的数据，并赋给原始的二维数组即可。
+         */
+        int chessArr2[][] = new int[sparseArr[0][0]][sparseArr[0][1]];
+        for (int i = 1; i < sparseArr.length; i++) {
+            chessArr2[sparseArr[i][0]][sparseArr[i][1]] = sparseArr[i][2];
+        }
+        //输出恢复后的二维数组
+        System.out.println();
+        System.out.println("恢复后的二维数组");
+        for (int[] row : chessArr2) {
+            for (int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
 
     }
 
